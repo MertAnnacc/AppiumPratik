8ebd4f48478e84d5b2ba508235bfc85d7ea7b066
MertAnnacc <mertannac91@gmail.com>
2023-05-19 16:44:09 +0300

    19.05.2023 mert2

---
+++ src/test/java/tests/apkYukleme.java
@@ -0,0 +1,44 @@
+package tests;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class apkYukleme {
+    AndroidDriver<MobileElement> driver;
+
+    @BeforeTest
+
+    public  void deneme() throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Mert2");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        //capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Mert\\IdeaProjects\\Appium\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
+
+        driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+
+    }
+
+    @Test
+
+    public void yukle(){
+        driver.activateApp("com.google.android.calculator");
+
+    }
+
+}
