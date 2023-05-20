e356b006e4bbf9cb8236b69dfdf575c6a50cd637
MertAnnacc <mertannac91@gmail.com>
2023-05-20 15:14:52 +0300

    20.05.2023 mert1

--- src/test/java/tests/ArabamTest.java
+++ src/test/java/tests/ArabamTest.java
@@ -1,15 +1,21 @@
 package tests;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class ArabamTest {
@@ -36,7 +42,75 @@ public class ArabamTest {
     }
 
     @Test
-    public void  test(){
+    public void  test() throws InterruptedException {
+        // Arabam kac para bolumune tiklayalim
+        driver.findElement(By.xpath("//*[@text='Arabam kaç para?']")).click();
+        // Aracimin fiyatini merak ediyorum bolumunetiklayalim
+        driver.findElement(By.xpath("//*[@text='Aracımın fiyatını merak ediyorum']")).click();
+        // Wolkswagen markasini secelim
+        TouchAction touchAction = new TouchAction<>(driver);
+
+        touchAction.press(PointOption.point(537,2053))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+                .moveTo(PointOption.point(531,649)).release().perform();
+
+        Thread.sleep(1000);
+
+        driver.findElementByXPath("//*[@text='Volkswagen']").click();
+
+        // yil secimi yapalim
+        driver.findElement(By.xpath("//*[@text='2020']")).click();
+
+        // model secimi yapalim
+        driver.findElement(By.xpath("//*[@text='Caddy']")).click();
+
+        // govde tipini secelim
+        driver.findElement(By.xpath("//*[@text='Panelvan']")).click();
+
+        // yakit tipini secelim
+        driver.findElement(By.xpath("//*[@text='Dizel']")).click();
+
+        // vites tipini secelim
+        driver.findElement(By.xpath("//*[@text='Düz']")).click();
+
+        // Versiyon secimi yapalim
+        driver.findElement(By.xpath("//*[@text='Seç']")).click();
+
+        // aracin km bilgilerini girelim
+        if(driver.isKeyboardShown()){
+            driver.getKeyboard().pressKey("80000");
+        } else{
+            driver.findElementById("com.dogan.arabam:id/et_km").sendKeys("80000");
+        }
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//*[@text='Devam']")).click();
+
+        // aracin rengini secelim
+        driver.findElement(By.xpath("//*[@text='Füme']")).click();
+
+        // opsiyel donanim (varsa) seecelim
+        driver.findElementById("com.dogan.arabam:id/iv_B01201").click();
+        driver.findElement(By.xpath("(//*[@text='Boyalı'])[2]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@text='Devam']")).click();
+
+        // degisen bilgisi ekleyerek tramer kaydi belirtelim
+        driver.findElement(By.xpath("//*[@text='Tramer kaydı var']")).click();
+        if(driver.isKeyboardShown()){
+            driver.getKeyboard().pressKey("5000");
+        } else{
+            driver.findElementById("com.dogan.arabam:id/et_km").sendKeys("5000");
+        }
+
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@text='Devam']")).click();
+
+        // Aradığınız aracın fiyatını bulamadık yazısının göründüğünü test edelim
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@text='Aradığınız aracın fiyatını bulamadık']")).isDisplayed());
+
+        // uygulamayi kapatalim
+        driver.closeApp();
 
 
     }
